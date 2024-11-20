1fd26016832872bcdcb3de5639a7df2f5799fb13
heloizas-beep <141248007+heloizas-beep@users.noreply.github.com>
2024-11-20 20:34:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Ruby?',
+    'O Ruby é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Austrália?',
+    'A capital da Austrália é Camberra'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma orientação a objetos (OO)?',
+    'É um paradigma de programação baseado em objetos, que combinam dados (atributos) e comportamentos (métodos).'
+)
+
+criaCartao(
+    'Lingua Russa',
+    'Como se diz Bom dia em Russo?',
+    'Bom dia em Russo é Доброе утро (pronuncia-se "Dóbraye útra").'
+)
